d602bc207f5f65c31651b33bdc8bc716021b5e91
Sugimoto Miki <miki.sugimoto@rakus-partners.co.jp>
2022-02-07 23:33:51 +0900

    ログイン画面表示機能が完成しました
    ログイン機能のLoginFormクラスを作成しました
    
    Controllerクラスにログインフォワードメソッドを追加しました
    
    ログイン画面を作成しました
    
    ログイン画面のhtmlを修正しました

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.form.LoginForm;
 import jp.co.sample.service.AdministratorService;
 
 @Controller
@@ -48,5 +49,14 @@ public class AdministratorController {
 		return "redirect:/";
 	}
 	
+	@ModelAttribute
+	public LoginForm setUpLoginForm() {
+		return new LoginForm();
+	}
+	
+	@RequestMapping("/")
+	public String toLogin() {
+		return "administrator/login";
+	}
 	
 }
