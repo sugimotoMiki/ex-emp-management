0a8bfa4bb609753e3832520a5987eb110077e459
Sugimoto Miki <miki.sugimoto@rakus-partners.co.jp>
2022-02-07 17:54:16 +0900

    管理者登録機能が完成しました。
    ServiceClassを作成しました
    
    controllerClassを更新しました
    
    controllerClassを更新しました
    
    管理者登録機能を作成しました

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,15 +1,22 @@
 package jp.co.sample.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.service.AdministratorService;
 
 @Controller
 @RequestMapping("/")
 public class AdministratorController {
 
+	@Autowired
+	private AdministratorService administratorService;
+	
 	/**
 	 * フォームオブジェクトをrequestスコープに格納
 	 * @return フォームオブジェクトがリクエストスコープに格納されます
@@ -26,6 +33,19 @@ public class AdministratorController {
 	public String toInsert() {
 		
 		return "administrator/insert";
+	}
+	
+	/**
+	 * 管理者情報を登録します
+	 * @param form
+	 * @return ログイン画面にリダイレクトします
+	 */
+	@RequestMapping("/insert")
+	public String insert(InsertAdministratorForm form) {
+		Administrator administrator = new Administrator();
+		BeanUtils.copyProperties(form, administrator);
+		administratorService.insert(administrator);
+		return "redirect:/";
 	}
 	
 	
